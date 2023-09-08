13a6a8c90903c95d44b7bc9ee0d411899e4c61c3
Ross McDermott <mcdermottr@gmail.com>
2023-09-09 00:05:17 +1000

    Refactor

--- http/handlers/health.go
+++ http/handlers/health.go
@@ -49,28 +49,6 @@ func (a *Health) process(w http.ResponseWriter, r *http.Request) {
 
 }
 
-// func RegisterRoutes(router chi.Router, logger *slog.Logger) {
-
-// 	logger.Debug("Registering '/health' route.")
-
-// 	router.Group(func(r chi.Router) {
-
-// 		r.Get("/healthz", func(w http.ResponseWriter, r *http.Request) {
-
-// 			logger.Debug("Health called")
-
-// 			data := status{
-// 				Ok: true,
-// 			}
-
-// 			w.Header().Set("Content-Type", "application/json")
-// 			json.NewEncoder(w).Encode(data)
-
-// 		})
-// 	})
-
-// }
-
 type status struct {
 	Ok bool `json:"ok"`
 }
